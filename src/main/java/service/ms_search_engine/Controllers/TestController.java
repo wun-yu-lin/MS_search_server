af30c9dbd4766ae9204990700db5251628347261
wun-yu-lin <76545619+wun-yu-lin@users.noreply.github.com>
2023-10-10 12:35:53 +0800

    RequestPara, body, header, PathVariable practice

--- src/main/java/service/ms_search_engine/Controllers/TestController.java
+++ src/main/java/service/ms_search_engine/Controllers/TestController.java
@@ -1,6 +1,6 @@
 package service.ms_search_engine.Controllers;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.StandardReflectionParameterNameDiscoverer;
+import org.springframework.web.bind.annotation.*;
 import service.ms_search_engine.Spectrum;
 
 @RestController
@@ -20,5 +20,37 @@ public class TestController {
         spectrum.setFormula("C2H6OH");
         return spectrum;
     }
+
+    @RequestMapping(value = "/getMethod", method = RequestMethod.GET)
+    @GetMapping("/getMethods")
+    public String get_parameter(@RequestParam(required = false) String message, // required = false 代表不一定要賦予參數數值
+                                @RequestParam(defaultValue = "test_user") String name){
+        System.out.println("message: " + message);
+        System.out.println("author: " + name);
+        return "message: " + message;
+
+    }
+    @RequestMapping("/getMethod/{id}/{name}")
+    public String get_parameter_by_id(@PathVariable int id,
+                                      @PathVariable String name ){
+        System.out.println("Id: " + id);
+        System.out.println("Name: " + name);
+        return "Id: " + id;
+
+    }
+
+    @RequestMapping("postMethod")
+    public String get_parameter(@RequestBody Spectrum spectrum,
+                                @RequestHeader String headerName,
+                                @RequestHeader(name="Content-type") String contentType
+                                ){
+        System.out.println("Header name: "+ headerName);
+        System.out.println("content-type: "+ contentType);
+        System.out.println(spectrum.getAuthorName());
+        return "postMethods";
+    }
+
+
+
 
 }
