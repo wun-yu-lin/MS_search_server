82843f7d0aaf5e2f7b1b4c35545a86b6fbda1b3c
wun-yu-lin <76545619+wun-yu-lin@users.noreply.github.com>
2023-12-07 02:24:50 +0800

    Web statu api, login  status icon, task submit auth

--- src/main/java/service/ms_search_engine/SecurityConfiguration.java
+++ src/main/java/service/ms_search_engine/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
