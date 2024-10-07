132197403b967e974d4fe040f8692b6e386bd2fe
wun-yu-lin <76545619+wun-yu-lin@users.noreply.github.com>
2024-10-07 20:44:02 +0800

    針對 be page & API 做身份認證 使用 AOP 處理

--- src/main/java/service/ms_search_engine/config/SecurityConfiguration.java
+++ src/main/java/service/ms_search_engine/config/SecurityConfiguration.java
@@ -45,7 +45,6 @@ public class SecurityConfiguration extends BaseConfig {
                         //static page
                         .requestMatchers(HttpMethod.GET, "/batchSearch", "/taskView", "/OAuthSuccess").authenticated()
                         .requestMatchers(HttpMethod.GET, "/be/**").authenticated()
-                        .requestMatchers(HttpMethod.POST, "/be/**").authenticated()
                         .requestMatchers(HttpMethod.GET, "/","/msSearch").permitAll()
                         .requestMatchers("/login").permitAll()
                         .requestMatchers("/logIn").permitAll()
